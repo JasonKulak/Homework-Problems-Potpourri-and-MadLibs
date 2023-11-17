a25b22e144bb952f5cff0a2cc901d9206db231cf
Jason Kulak <JasonKulak@yahoo.com>
2023-11-17 09:36:11 -0600

    Count Odd Numbers. Still working on it

--- solutions.js
+++ solutions.js
@@ -1,28 +1,38 @@
-//SET ALARM//
+///////////
+//SET ALARM
+///////////
 //Write a function named setAlarm that has two arguments:
 //The first argument: employed (the boolean true would be passed in as a parameter whenever you are employed)
 //The second argument: vacationing (the boolean true would be passed in as a parameter whenever you are on vacation)
 
 
-function setAlarm(employed, vacationing){
-    if (employed === true && vacationing === false){
-        console.log(true)
-    } else {
-        console.log(false)
-    }
-}   
-setAlarm(true, true)
+// function setAlarm(employed, vacationing){
+//     if (employed === true && vacationing === false){
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }   
+// setAlarm(true, true)
 
 //setAlarm(true, true) // => returns false
 //setAlarm(false, false) // => returns false
 //setAlarm(false, true) // => returns false
 //setAlarm(true, false) // => returns true
 
-
-//Count Odd Numbers//
-
-function oddNumberCount = (number) => {
-    for (i = 0; i <= number; i++)
-    i % 2 !== 0;
-    console.log(i)
+///////////////////
+//Count Odd Numbers
+///////////////////
+//COME BACK TO THIS?
+function oddNumberCount(number){
+for (let i = 0; i <= number; i++) {
+    if (i % 2 != 0) {
+        console.log(i)
+         }
+    }
 }
+oddNumberCount(10)
+
+//////////////////////
+//Disemvoweling Trolls
+//////////////////////
