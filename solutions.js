d4f0553b40b41130d454abdeacba5e62bfd5e80b
Jason Kulak <JasonKulak@yahoo.com>
2023-11-22 20:49:02 -0600

    Count Odd Numbers and homework complete

--- solutions.js
+++ solutions.js
@@ -6,14 +6,14 @@
 //The second argument: vacationing (the boolean true would be passed in as a parameter whenever you are on vacation)
 
 
-// function setAlarm(employed, vacationing){
-//     if (employed === true && vacationing === false){
-//         console.log(true)
-//     } else {
-//         console.log(false)
-//     }
-// }   
-// setAlarm(true, true)
+function setAlarm(employed, vacationing){
+    if (employed === true && vacationing === false){
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}   
+setAlarm(true, true)
 
 //setAlarm(true, true) // => returns false
 //setAlarm(false, false) // => returns false
@@ -25,24 +25,28 @@
 ///////////////////
 //COME BACK TO THIS?
 
-// function oddNumberCount(num) {
-// for (let i = 0; i <= num.length; i++){
-//     if (i % 2 !==0)
-    
-//     }
-// }
-// oddNumberCount(10)
+let numCount = 0
+function oddNumberCount(number) {
+   
+for (let i = 1; i < number; i+=2){
+       numCount++
+    }
+    return numCount
+}
+console.log(oddNumberCount(7))
+
+
 
 //////////////////////
 //Disemvoweling Trolls
 //////////////////////
-//Write a function that returns a string with no vowels
-// function trollsBeGone() {
-//     const Bilbo = "I'll turn you all into stone!"
-//     const noVowels = Bilbo.replace(/[aeiou]/gi, '');
-//     console.log(noVowels)
-// }
-// trollsBeGone()
+Write a function that returns a string with no vowels
+function trollsBeGone() {
+    const Bilbo = "I'll turn you all into stone!"
+    const noVowels = Bilbo.replace(/[aeiou]/gi, '');
+    console.log(noVowels)
+}
+trollsBeGone()
 
 
 //////////////////////
